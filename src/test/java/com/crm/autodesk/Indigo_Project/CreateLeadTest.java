79f0d5ab8faef3547f6323fb02705abfe040c7b3
soumya lall <soumya lall@LAPTOP-CU2M7T7J>
2022-03-24 18:02:17 +0530

    user1 firstcommit

---
+++ src/test/java/com/crm/autodesk/Indigo_Project/CreateLeadTest.java
@@ -0,0 +1,11 @@
+package com.crm.autodesk.Indigo_Project;
+
+import org.testng.annotations.Test;
+
+public class CreateLeadTest {
+	@Test
+	public void createlead()
+	{
+	System.out.println("lead is created ");
+}
+}
---
+++ src/test/java/com/crm/autodesk/Indigo_Project/package-info.java
@@ -0,0 +1,1 @@
+package com.crm.autodesk.Indigo_Project;
